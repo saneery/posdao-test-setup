7e265529b2e958986f7e6242ed492407d536bcb7
Andreas Fackler <AndreasFackler@gmx.de>
2019-02-11 15:33:21 +0100

    WIP

--- test/token.js
+++ test/token.js
@@ -1,12 +1,13 @@
 const Token = artifacts.require('ERC677BridgeTokenRewardableMock')
+const constants = require("../utils/constants");
 
 const ValidatorSetContract = require("../utils/getContract")("ValidatorSetAuRa", web3);
 
 contract('TestToken', _accounts => {
-  it('should have 0 initial supply', async () => {
+  it('should have 2000 initial supply', async () => {
     var instance = await Token.deployed()
     var supply = await instance.totalSupply.call()
-    assert.equal(supply.valueOf(), 0, "the initial supply isn't 0")
+    assert.equal(supply.valueOf(), web3.utils.toWei("2000"), "the initial supply isn't 2000")
   })
 
   it('validatorSetContract field value should match ValidatorSet contract address', async () => {
@@ -19,5 +20,29 @@ contract('TestToken', _accounts => {
     let instance = await Token.deployed();
     let erc20TokenContract = await ValidatorSetContract.instance.methods.erc20TokenContract().call();
     assert.equal(erc20TokenContract.valueOf(), instance.address);
+  });
+
+  it('validator has 1000', async () => {
+    var instance = await Token.deployed()
+    var balance = await instance.balanceOf.call(constants.VALIDATOR1)
+    assert.equal(balance.valueOf(), web3.utils.toWei("1000"), "the validator doesn't have 1000")
+  })
+
+  it('can stake', async () => {
+    var minStake = await ValidatorSetContract.instance.methods.getCandidateMinStake().call()
+    assert.equal(minStake.valueOf(), web3.utils.toWei("1"), "the min stake isn't 1")
+    var allowed = await ValidatorSetContract.instance.methods.areStakeAndWithdrawAllowed().call()
+    assert.equal(allowed.valueOf(), true, "not allowed")
+    let opts = {
+      from: constants.VALIDATOR1,
+      gasPrice: "1",
+      gas: "2000000",
+    };
+    try {
+      await ValidatorSetContract.instance.methods.stake(constants.VALIDATOR1, web3.utils.toWei("1000")).send(opts);
+    } catch (err) {
+      console.log(err);
+      assert.equal(0, 1);
+    }
   });
 })
--- utils/constants.js
+++ utils/constants.js
@@ -3,5 +3,6 @@ const VALIDATOR_SET_ADDRESS = require("../posdao-contracts/spec").engine.authori
 
 module.exports = {
   OWNER: "0x32e4e4c7c5d1cea5db5f9202a9e4d99e56c91a24",
+  VALIDATOR1: "0xbbcaa8d48289bb1ffcf9808d9aa4b1d215054c78",
   VALIDATOR_SET_ADDRESS,
 };
